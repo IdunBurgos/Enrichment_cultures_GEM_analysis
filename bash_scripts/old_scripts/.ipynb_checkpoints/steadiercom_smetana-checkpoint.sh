5e8b6f81675127b325f422d1bb2f6d5d8a527a0b
IdunBurgos <idun.m.t.burgos@ntnu.no>
2024-06-21 15:24:50 +0200

    deleted more jupyternotebook checkpoints

--- bash_scripts/old_scripts/.ipynb_checkpoints/steadiercom_smetana-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Running smetana for communities in different SynCon medias and on different carbon substrates
-
-set -eux
-
-
-steadiercom ./output/GEMs/GEMs_adapt_media_ACt2r/*.xml -c ./output/smetana/communities_top99.tsv -m SC2_X,SC2_C --mediadb ./output/smetana/media_db.tsv -o ./output/smetana_steadiercom/top99 --sample 50
-
--- functions/.ipynb_checkpoints/colors_community-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import colors
-
-def cmap_community():
-    # red mid color FF7074
-    # green mid color 50C878
-    cs = [ "red","#F88484","#F4CCCC","#00A36C","#9FD787","#D9EAD3","#6497b1","#b9d7f2"]
-    cmap = colors.ListedColormap( cs)
-    return cmap
